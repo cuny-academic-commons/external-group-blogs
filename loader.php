edfcf26e5fb96015d5a88c05f1af507d4b13a120
r-a-y <r-a-y@gmx.com>
2015-04-13 13:51:06 -0700

    Port loader.php changes from CAC version.

--- loader.php
+++ loader.php
@@ -2,20 +2,29 @@
 /*
 Plugin Name: External Group Blogs
 Plugin URI: http://wordpress.org/extend/plugins/external-group-blogs/
-Description: Allow group creators to supply external blog RSS feeds that will attach future posts on blogs to a group.
+Description: Allows group creators to supply external blog RSS feeds that will attach future posts on blogs to group activity.
 Version: 1.5.2
-Requires at least: WordPress 2.9.1 / BuddyPress 1.5
-Tested up to: WordPress 3.5 / BuddyPress 1.6.3
+Requires at least: WordPress 3.5 / BuddyPress 1.7
+Tested up to: WordPress 3.5 / BuddyPress 1.7
 License: GNU/GPL 2
-Author: Andy Peatling, modemlooper, boonebgorges, cuny-academic-commons
+Author: Andy Peatling
+Contributors: apeatling, modemlooper, boonebgorges
 Author URI: http://buddypress.org/developers/apeatling/
 */
 
 /* Only load the plugin functions if BuddyPress is loaded and initialized. */
 function bp_groupblogs_init() {
-	require( dirname( __FILE__ ) . '/bp-groups-externalblogs.php' );
+	require( dirname( __FILE__ ) . '/includes/bp-groups-externalblogs.php' );
 }
 add_action( 'bp_init', 'bp_groupblogs_init' );
+
+
+/*** Load translations for this plugin */
+function bp_groupblogs_load_translations() {
+	load_plugin_textdomain( 'bp-groups-externalblogs', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'bp_groupblogs_load_translations' );
+
 
 /* On activation register the cron to refresh external blog posts. */
 function bp_groupblogs_activate() {
@@ -23,9 +32,11 @@ function bp_groupblogs_activate() {
 }
 register_activation_hook( __FILE__, 'bp_groupblogs_activate' );
 
+
 /* On deacativation, clear the cron. */
 function bp_groupblogs_deactivate() {
 	wp_clear_scheduled_hook( 'bp_groupblogs_cron' );
+
 	/* Remove all external blog activity */
 	if ( function_exists( 'bp_activity_delete' ) )
 		bp_activity_delete( array( 'type' => 'exb' ) );
